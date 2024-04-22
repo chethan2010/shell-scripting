f4b9e17650946020b8a793e86c07a6320daa98ec
Hemanth <hemanthkumarsaketi@gmail.com>
2024-04-22 13:44:37 +0530

    varableschng

--- Varables.sh
+++ Varables.sh
@@ -1,1 +1,5 @@
-echo "This is Varables shellfile"
+#!/bin/bash
+echo "Ramesh, Hello Ramesh how are you?"
+echo "Suresh, Hi Ramesh how are you?"
+echo "Ramesh, Iam fine too.how is your work?"
+echo "Kumar, Not bad Ramesh Thinking to upgrade Devops?" 
