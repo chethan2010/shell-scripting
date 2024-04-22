c38d04aad18affc9812390e9a41250ed9c3676ef
Hemanth <hemanthkumarsaketi@gmail.com>
2024-04-22 16:16:46 +0530

    changes done

---
+++ 03-varables.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+PERSON1=SHANKAR
+PERSON2=RAJU
+PERSON3=KRISHNA
+PERSON4=KAMLASH
+echo "$PERSON1, Hello Ramesh how are you?"
+echo "$PERSON2, Hi Ramesh how are you?"
+echo "$PERSON3, Iam fine too.how is your work?"
+echo "$PERSON4, Not bad Ramesh Thinking to upgrade Devops?" 
+echo "Kranthi, Iam fine too.how is your work?"
