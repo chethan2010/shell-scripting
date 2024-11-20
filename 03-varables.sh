952637e5c6dad3dfd7f4ebe2ae0b2f6fdc3d642c
chethan2010 <hemanthkumarsaketi@gmail.com>
2024-11-20 14:59:50 +0530

    shell-script

--- 03-varables.sh
+++ 03-varables.sh
@@ -1,11 +1,13 @@
 #!bin/bash
-
+#declaring variables
 PERSON1=ChandrababuNaidu
 PERSON2=KCR
 PERSON3=Jagan
 PERSON4=Kalyan
+
+#Refrencing varables
 echo "$PERSON1, Hello Ramesh how are you?"
 echo "$PERSON2, Hi Ramesh how are you?"
 echo "$PERSON3, Iam fine too.how is your work?"
 echo "$PERSON4, Not bad Ramesh Thinking to upgrade Devops?" 
-echo "Kranthi, Iam fine too.how is your work?"
+echo "$PERSON1, Iam fine too.how is your work?"
