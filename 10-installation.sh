f4ed65625dd8fdfbe6f6e06c84ffa6a691067f08
chethan2010 <hemanthkumarsaketi@gmail.com>
2024-11-21 08:34:51 +0530

    shell-script

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-sudo dnf install mysql -y
+
+USERID=$(id -u)
+if [ $USERID  -eq 0 ]
+then 
+ echo "you are super user"
+ else
+ echo "you are not super user"
+ fi 
+
+
+
+#sudo dnf install mysql -y
+
+
