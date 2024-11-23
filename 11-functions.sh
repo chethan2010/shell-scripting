32985b8ea0e8af38452311e718087817b11e2ba9
chethan2010 <hemanthkumarsaketi@gmail.com>
2024-11-23 06:03:06 +0530

    shell-script

--- 11-functions.sh
+++ 11-functions.sh
@@ -6,7 +6,7 @@ TIMESTAMP=$(date +%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 VALIDATE(){
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ]
     then
         echo "$2....Failure"
         exit 1
