1b1e4dc6096da2cec9e209f8aaa9c81737e7d34c
chethan2010 <hemanthkumarsaketi@gmail.com>
2024-11-23 06:32:41 +0530

    shell-script

---
+++ 12-colours.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+USERID=$(id -u)
+TIMESTAMP=$(date +%H-%M-%S)
+SCRIPT_NAME=$(echo 0 | cut '.' -f )
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please Run script as Super User "
+        exit 1
+    else 
+        echo "You are a Super User"
+    fi
+}
+
+VALIDATE(){
+    if [ $1 - ne 0]
+    then
+        echo  "$2 ...........Failure"
+        exit 1
+    else
+        echo "$2 .............Success"
+    fi
+
+}
+
+dnf install mysql -y
+VALIDATE $? "INSTALLING MYSQL"
+
+dnf install git -y
+VALIDATE $? "INSTALLING git"
