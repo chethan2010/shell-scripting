97be689699c0a960f2d3803e2fd1bf8a1eeb3b62
chethan2010 <hemanthkumarsaketi@gmail.com>
2024-11-27 13:37:00 +0530

    shell-script

--- 14-installpackages.sh
+++ 14-installpackages.sh
@@ -1,6 +1,20 @@
 #!bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%H-%M-%S)
+SCRIPT_NAME=$(echo 0 | cut '.' -f1 )
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2....Failure"
+        exit 1
+    else
+        echo "$2...Success"
+    fi
+}
+
 
 if [ $USERID -ne 0]
 then
@@ -10,6 +24,15 @@ else
     echo "You are a Super user"
 fi
 
-echo "all packages:$@"
+for $i in $@
+do
+    echo "Packages to install: $i"
+    dnf list installed $i &>>$LOGFILE
+fi
 
 
+if [ $? -ne 0 ]
+then 
+    echo "$i is already installed ..... skipping"
+else
+    echo  "$i is not installed need to be installed"
