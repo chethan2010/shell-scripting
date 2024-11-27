b8a3b10b963c3609dea98d213b359da2adb88a66
chethan2010 <hemanthkumarsaketi@gmail.com>
2024-11-27 15:57:38 +0530

    shell-script

--- 14-installpackages.sh
+++ 14-installpackages.sh
@@ -4,19 +4,24 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%H-%M-%S)
 SCRIPT_NAME=$(echo 0 | cut '.' -f1 )
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
+
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2....Failure"
+        echo -e "$2....$R Failure $N"
         exit 1
     else
-        echo "$2...Success"
+        echo "$2..... $G Success $G"
     fi
 }
 
 
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
     echo "please run script as super user"
     exit 1
@@ -24,16 +29,16 @@ else
     echo "You are a Super user"
 fi
 
-for $i in $@
+for i in $@
 do
     echo "Packages to install: $i"
     dnf list installed $i &>>$LOGFILE
 
-if [ $? -ne 0 ]
-then 
-    echo "$i is already installed ..... skipping"
-else
-    echo  "$i is not installed need to be installed"
+    if [ $? -eq 0 ]
+    then 
+        echo "$i is already installed ..... $Y skipping $N"
+    else
+        echo  "$i is not installed need to be installed"
 
-fi 
+    fi 
 done
