4a14a27051e2c90cff52c9e01eac0bd6626eefc2
chethan2010 <hemanthkumarsaketi@gmail.com>
2024-11-27 15:59:33 +0530

    shell-script

--- 14-installpackages.sh
+++ 14-installpackages.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%H-%M-%S)
-SCRIPT_NAME=$(echo 0 | cut "." -f1 )
+SCRIPT_NAME=$(echo $0 | cut "." -f1 )
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
