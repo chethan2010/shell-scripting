1b6f50dcbfd4c64a77bdf9218dba9997668b2576
unknown <trustfixhomeservices@gmail.com>
2026-05-13 18:09:08 +0530

    var

--- 03-varables.sh
+++ 03-varables.sh
@@ -1,13 +1,9 @@
 #!bin/bash
 #declaring variables
-PERSON1=ChandrababuNaidu
-PERSON2=KCR
-PERSON3=Jagan
-PERSON4=Kalyan
+PERSON1=Rajesh
+PERSON2=Ravi
+
 
 #Refrencing varables
-echo "$PERSON1, Hello Ramesh how are you?"
-echo "$PERSON2, Hi Ramesh how are you?"
-echo "$PERSON3, Iam fine too.how is your work?"
-echo "$PERSON4, Not bad Ramesh Thinking to upgrade Devops?" 
-echo "$PERSON1, Iam fine too.how is your work?"
+echo "Hello $PERSON2 how are you?"
+echo "Hi $Rajesh Iam Good how are you?"
