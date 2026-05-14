3daff917cfb729b09b465b92a38acab17b3925ea
unknown <trustfixhomeservices@gmail.com>
2026-05-14 15:08:32 +0530

    var

--- 03-varables.sh
+++ 03-varables.sh
@@ -1,9 +1,11 @@
-#!bin/bash
-#declaring variables
-PERSON1=Rajesh
-PERSON2=Ravi
+#!/bin/bash
 
+#declaring of varible
+PERSON1=Sachin #no space between = and value
+PERSON2=Rahul
 
-#Refrencing varables
-echo "Hello $PERSON2 how are you?"
-echo "Hi $PERSON2 Iam Good how are you?"
+#referring variable
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "${PERSON2}:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am fine too. how is your work?"
+echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
